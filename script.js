fd48bfacf2d5754ee7bad2a228b9e8f757a6462b
Sbee8777 <seb.bastiaans@hva.nl>
2025-12-09 17:29:18 +0100

    #6 komma's verwijdert JS

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // 1 - selecteer search de button
 let searchAiButton = document.querySelector('.ai-button-secondary')
-console.log(searchAiButton)
 
 // 2 - event click
 searchAiButton.addEventListener('click',
 // 3 - display bock worden. + h1 op display none
 function(){
-    document.querySelector('main-section-search, article').classList.add('article-visible')
-    document.querySelector('main-section-search, h1').classList.add('heading-gone')
+    document.querySelector('#open-AI-chat').classList.add('article-visible')
+    document.querySelector('h1').classList.add('heading-gone')
 }
 )
 // document.querySelector('.main-section-search article')
