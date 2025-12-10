54784a7b185afb2a0c0c5df37b567aa8b3da0f9d
Sbee8777 <seb.bastiaans@hva.nl>
2025-12-10 12:00:12 +0100

    email input kleur bij invalid

--- script/script.js
+++ script/script.js
@@ -1,12 +1,22 @@
 // 1 - selecteer search de button
 let searchAiButton = document.querySelector('.ai-button-secondary')
+let inputAiEnter = document.querySelector('.secondary-search-input')
 
 // 2 - event click
 searchAiButton.addEventListener('click',
-// 3 - display bock worden. + h1 op display none
+
+    // 3 - display bock worden. + h1 op display none
 function(){
     document.querySelector('#open-AI-chat').classList.add('article-visible')
     document.querySelector('h1').classList.add('heading-gone')
 }
 )
 // document.querySelector('.main-section-search article')
+
+inputAiEnter.addEventListener('keydown', function(event){
+    console.log('jooooo')
+    if (event.key === 'Enter'){
+        document.querySelector('#open-AI-chat').classList.add('article-visible')
+        document.querySelector('h1').classList.add('heading-gone')
+    }
+})
