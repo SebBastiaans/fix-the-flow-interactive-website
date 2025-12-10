e785938e47e70ba4c274244c53d3dc08f7de6226
Sbee8777 <seb.bastiaans@hva.nl>
2025-12-10 17:02:34 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,14 +1,17 @@
-// 1 - selecteer search de button
+// 1 - Zoek element in het document. (button, input)
+// 2 - Luister naar een event, en roep een callback function aan. (click, submit)
+// 3 - pas een property van een element aan (classlist)
+
+//chat openen met button
 let searchAiButton = document.querySelector('.ai-button-secondary')
-let inputAiEnter = document.querySelector('.secondary-search-input')
-let chatDown = document.querySelector('#chat-handle')
-// 2 - event click
-// 3 - display bock worden. + h1 op display none
+
 searchAiButton.addEventListener('click', function(){
     document.querySelector('#open-AI-chat').classList.add('article-visible')
     document.querySelector('h1').classList.add('heading-gone')
 }
 )
+//chat dichtklappen
+let chatDown = document.querySelector('#chat-handle')
 
 chatDown.addEventListener('click', function(){
     document.querySelector('#open-AI-chat').classList.remove('article-visible')
@@ -16,6 +19,7 @@ chatDown.addEventListener('click', function(){
 
 })
 
+//chat openen met enter knop
 let form = document.querySelector('.secondary-search-bg')
 
 form.addEventListener('submit', function(event) {
